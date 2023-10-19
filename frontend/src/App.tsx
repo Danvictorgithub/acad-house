477fdacc4e37ac6bd252dff8e896c18880d228a5
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-10-19 22:58:28 +0800

    Added a horizontal slider for type of housing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,32 @@ function App() {
           <img src="logo.png" className='h-16'></img>
           <h1 className='text-2xl font-bold text-blue-700'>acadhouse</h1>
         </div>
+        <div className='w-[500px] flex items-center border-2 rounded-full overflow-hidden p-2'>
+            <span className="material-symbols-outlined">more_vert</span>
+            <input type="text" className='w-full' placeholder='Enter Location'/>
+            <span className="material-symbols-outlined bg-blue-700 text-white p-[5px] rounded-full">search</span>
+        </div>
         <div className="w-full flex justify-end">
           <div className='flex items-center justify-center gap-2 border-2 px-4 py-1 rounded-full'>
             <p className='text-xl font-medium text-zinc-700'>login</p>
             <span className="material-symbols-outlined text-4xl text-zinc-700">account_circle</span>
+          </div>
+        </div>
+      </div>
+      <div className='p-16 flex flex-col h-full'>
+        <div className='flex gap-4 overflow-auto whitespace-nowrap snap-mandatory no-scrollbar'>
+          {[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5].map(() => {
+            return (
+            <div className="flex flex-col justify-center items-center snap-start">
+              <span className="material-symbols-outlined">night_shelter</span>
+              <p>Boarding House</p>
+            </div>
+            )
+          })}
+        </div>
+        <div className='grid'>
+          <div>
+
           </div>
         </div>
       </div>
